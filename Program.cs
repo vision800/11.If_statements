ea196f5bbffe906417e7bcc49cd17ab7b73a0ff0
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:03:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.If_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare 
+            bool isMale = true;
+
+            if (isMale)
+            {
+                Console.WriteLine("you are male");
+            }else
+            {
+                Console.WriteLine("you are female");
+            }
+
+            bool isTall = false;
+
+            if (isTall && isMale) {
+                Console.WriteLine("you  are tall and male");
+            }else if (!isTall && isMale) {
+                Console.WriteLine("you are not tall but you are male");
+                    }else { Console.WriteLine("you are either not tall or  not male"); }
+
+        }
+    }
+}
